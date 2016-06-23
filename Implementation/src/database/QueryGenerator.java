42ba4b2464eedd36aaf58ab1fa67c294f8d9e254
Your <ppashakhanloo@gmail.com>
2016-06-23 15:09:06 +0430

    query generator added.

---
+++ Implementation/src/database/QueryGenerator.java
@@ -0,0 +1,50 @@
+package database;
+
+import java.util.ArrayList;
+
+public class QueryGenerator {
+	private String query;
+	public static QueryGenerator queryGenerator;
+
+	private QueryGenerator() {
+	}
+
+	public static QueryGenerator getInstance() {
+		if (queryGenerator == null)
+			queryGenerator = new QueryGenerator();
+		return queryGenerator;
+	}
+
+	public String select(String tableName, ArrayList<String> colNames, String cond) {
+		return "SELECT " + (colNames == null ? '*' : separatedList(",", "", colNames)) + " FROM " + tableName
+				+ (cond == null ? "" : " WHERE " + cond);
+	}
+
+	public String insert(String tableName, ArrayList<String> colNames, ArrayList<String> values) {
+		return "INSERT INTO " + tableName + " (" + separatedList(",", "", colNames) + ") VALUES ("
+				+ separatedList(",", "'", values) + ")";
+	}
+
+	private String separatedList(String separator, String wrapper, ArrayList<String> values) {
+		String stringValues = "";
+		for (String value : values)
+			stringValues += wrapper + value + wrapper + separator + " ";
+		stringValues = stringValues.substring(0, stringValues.length() - 2);
+		return stringValues;
+	}
+
+	public static void main(String[] args) {
+		String z = "ak";
+		String a = "no";
+
+		QueryGenerator generator = QueryGenerator.getInstance();
+		ArrayList<String> values = new ArrayList<>();
+		values.add(z);
+		values.add(a);
+		ArrayList<String> colNames = new ArrayList<>();
+		colNames.add("A");
+		colNames.add("B");
+		System.out.println(generator.select("student", colNames, null));
+		System.out.println(generator.insert("a", colNames, values));
+	}
+}
