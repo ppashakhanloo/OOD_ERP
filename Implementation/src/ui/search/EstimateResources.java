942ffa3bee701f287eb42ab1b1b1f77619bd5041
Mozhdeh <gheini72@gmail.com>
2016-07-03 14:22:03 +0430

    added new window: RequiredResourcesReport.

--- Implementation/src/ui/search/EstimateResources.java
+++ Implementation/src/ui/search/EstimateResources.java
@@ -132,6 +132,7 @@ public class EstimateResources implements Visibility {
                 OperationFacade.getInstance().search(tech, devCount, usercount).forEach(project -> listModelProj.addElement(project));
             }
         });
+        
         mainFrame.getMainFrame().pack();
         mainFrame.getMainFrame().setLocationRelativeTo(null);
     }
