098f5b57fddeb74324378219df7edb6a9a57eb4a
Your <ppashakhanloo@gmail.com>
2016-07-07 11:53:06 +0430

    add backup stuff

---
+++ Deployment/restore.php
@@ -0,0 +1,45 @@
+<?php
+
+/////////////////////////////////////////
+// DATABE CONFIGURATIONS ////////////////
+/////////////////////////////////////////
+$mysql_host = '127.0.0.1';
+$mysql_username = 'root';
+$mysql_password = '';
+$mysql_database = 'erp';
+/////////////////////////////////////////
+/////////////////////////////////////////
+/////////////////////////////////////////
+
+/////////////////////////////////////////
+// BACKUP FILE NAME /////////////////////
+/////////////////////////////////////////
+$filename = 'backp-file-name.sql';
+/////////////////////////////////////////
+/////////////////////////////////////////
+/////////////////////////////////////////
+
+mysql_connect($mysql_host, $mysql_username, $mysql_password) or die('Error connecting to MySQL server: ' . mysql_error());
+mysql_select_db($mysql_database) or die('Error selecting MySQL database: ' . mysql_error());
+
+
+mysql_query('SET foreign_key_checks = 0');
+mysql_query("SELECT Concat('TRUNCATE TABLE ',table_schema,'.',TABLE_NAME, ';') FROM erp.TABLES where  table_schema in ('erp');");
+
+$templine = '';
+$lines = file($filename);
+foreach ($lines as $line)
+{
+if (substr($line, 0, 2) == '--' || $line == '')
+    continue;
+
+$templine .= $line;
+if (substr(trim($line), -1, 1) == ';')
+{
+    mysql_query($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . mysql_error() . '<br /><br />');
+    $templine = '';
+}
+}
+	mysql_query('SET foreign_key_checks = 1');
+ echo "Resoratoring the backup file contents completed successfully.";
+?>
